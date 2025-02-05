ec8b9da7353e90c0f9d8d8c252a5fad000d279c6
leoaf01 <laramayo01@hotmail.com>
2025-02-04 22:45:45 -0500

    deleted most of main(), printed message

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,14 @@
 
 void InitializeBoard() {
     // sets all the pieces to their specific positions
+    std::cout << "Initializing Board" << std::endl;
+
+
 }
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({591, 591}), "My Chess Game");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    sf::Texture leo;
-    leo.loadFromFile("images/leo.jpg");
-
-    sf::Sprite sprite(leo);
-    sprite.setTextureRect(sf::IntRect({1, 1}, {591, 591}));
-    sprite.setPosition({0, 0});
-    
-
-    while (window.isOpen())rr
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(sprite);
-        window.display();
-    }
-
-    TextureManager::Clear();
+    InitializeBoard();   
     return 0;
 }
